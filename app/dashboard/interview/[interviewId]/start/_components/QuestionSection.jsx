e652aaf5dfa6cfddeecc43fedc645aa3b612081c
vaishnavgupta <158202421+vaishnavgupta@users.noreply.github.com>
2025-04-03 23:47:44 +0530

    Showing Que in UI

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import { Ear, Sun } from 'lucide-react';
+import React from 'react';
 
-function QuestionSection(activeQuestionIndex) {
+function QuestionSection({ mockIntvQues, activeQuestionIndex }) {
+
+  const textToSpeechConvertor=(text) => {
+    if('speechSynthesis' in window){
+      const speech = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech);
+    }
+    else{
+      alert('Speech Translation Unavailable!')
+    }
+  }
+
   return (
-    <div className='p-5 border rounded-lg'>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-            <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestionIndex}`}>
-                Questions No. 1
-
+    mockIntvQues && (
+      <div className="p-5 border rounded-lg bg-purple-100 mx-auto mt-1 w-fit">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+          {mockIntvQues.map((ques, idx) => (
+            <h2
+              key={idx} // ✅ Added key prop
+              className={`p-2 rounded-full text-xs md:text-sm text-center cursor-pointer mb-5 
+                ${activeQuestionIndex === idx ? 'bg-red-400 text-black' : 'bg-gray-200'}`}
+            >
+              Questions No. #{idx + 1}
             </h2>
+          ))}
         </div>
 
-    </div>
-  )
+        <h2 className="mt-4 font-bold text-purple-700 text-lg">
+          {mockIntvQues[activeQuestionIndex]?.Question}
+        </h2>
+
+        <Ear className='cursor-pointer mt-2' onClick={()=>textToSpeechConvertor(mockIntvQues[activeQuestionIndex]?.Question)}/>
+
+        <div className='border rounded-lg p-5 bg-green-300 mt-20'>
+          <h2 className='flex gap-2 items-center text-black'>
+            <Sun/>
+            <strong>Pro Tip!</strong>
+          </h2>
+          <h2>{process.env.NEXT_PUBLIC_INTERVIEW_PRO_TIP}</h2>
+        </div>
+
+      </div>
+    )
+  );
 }
 
-export default QuestionSection
+export default QuestionSection;
+
