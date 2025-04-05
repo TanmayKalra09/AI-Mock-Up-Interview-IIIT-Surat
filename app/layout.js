f84969a01aead1b314505deb7243a95c35f01ed6
Tanmay Kalra <tanmaykalra0910@gmail.com>
2025-04-05 17:52:44 +0530

    Resolving Errors

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
+  variable: "--font-geist-sans", 
   subsets: ["latin"],
 });
 
@@ -20,6 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/2944658_28388-removebg-preview.png" type="image/png"/>
+        <title>AI Mock Interview by Team Mavericks</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-      serverComponentsExternalPackages: ["@clerk/nextjs"], // Ensures compatibility
+      serverComponentsExternalPackages: ['@prisma/client'], // Ensures compatibility
     },
   };
   
